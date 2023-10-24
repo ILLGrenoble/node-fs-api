b72fb4d3fca1bb0031b6c94322eebda4131e26e2
Stuart Caunt <caunt@ill.fr>
2023-10-24 13:12:45 +0200

    Get case insensitive auth token

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -2,11 +2,19 @@ import { NextFunction, Request, Response } from "express";
 
 import { APPLICATION_CONFIG } from "../application-config";
 
+const getCaseInsensitiveHeader = (header: string, req: Request): string => {
+    const lowerCaseHeader = header.toLowerCase();
+    for (const requestHeader in req.headers) {
+        if (requestHeader.toLowerCase() === lowerCaseHeader) {
+            return req.header(requestHeader);
+        }
+    }
+}
+
 export function authTokenMiddleware(req: Request, res: Response, next: NextFunction) {
 
-    const xAuthToken = req.header('X-AUTH-TOKEN');
+    const xAuthToken = getCaseInsensitiveHeader('x-auth-token', req);
     const targetToken = APPLICATION_CONFIG().server.authToken;
-
 
     if (targetToken === '' || targetToken == null) {
         next();
